b5909b25e7c5624e13824fd78f37059d0d813c5e
pavelc4 <dimzzy23@gmail.com>
2025-10-06 23:44:44 +0700

    feat(comments): add example for rust comments
    - Create `src/bin/comment.rs` to demonstrate Rust comments
    - Include examples of single-line (`//`) and multi-line (`/* ... */`) comments
    - Provide a basic `main` function to print "hello" and "worlds"

---
+++ src/bin/comment.rs
@@ -0,0 +1,17 @@
+// coment satu baris  di rust 
+/*
+contoh comentar banyak baris di rust, kurang lebih seperti bahasa  pemerograman lainya 
+
+ Doc Contoh code hello world di rust dengan menempatkan suaatu nama di dalam variabel 
+*/
+
+
+fn main(){
+
+    let hello = "hello";
+    let worlds = "worlds";
+
+    println!("{}", hello);
+    println!("{}", worlds);
+
+}
