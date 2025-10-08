fcb7cd824dcc67b3d205366507230500225f5540
pavelc4 <dimzzy23@gmail.com>
2025-10-09 00:36:04 +0700

    refactor(tuple): simplify example and use destructuring
    - Remove `&str` element from `data` tuple
    - Comment out direct index access for tuple elements
    - Use tuple destructuring `let (a, b, c) = data` for better readability
    - Update `println!` macro to match the new tuple structure

--- src/bin/tuple.rs
+++ src/bin/tuple.rs
@@ -1,11 +1,14 @@
 fn main() {
-    let data: (i32, f64,bool , &str) = (10 ,10.5, true, "dimas");
-    println!("{:?}",data);
+    let data: (i32, f64, bool) = (10, 10.5, true);
+    println!("{:?}", data);
 
-    let a = data.0;
-    let b = data.1;
-    let c = data.2;
-    let name = data.3;
+    /* let a = data.0;
+     let b = data.1;
+     let c = data.2;
+     let name = data.3;
+    */
 
-    println!("{}{}{}{}", a, b, c, name);
+    let (a, b, c) = data;
+
+    println!("{}{}{}", a, b, c);
 }
