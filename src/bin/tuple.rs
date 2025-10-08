867a4216a6cbfe71bd4328b0d59dd0e770567b40
pavelc4 <dimzzy23@gmail.com>
2025-10-09 00:19:36 +0700

    doc (tuple) how to acsess tuple

--- src/bin/tuple.rs
+++ src/bin/tuple.rs
@@ -1,4 +1,11 @@
 fn main() {
-    let data: (i32, f64,bool) = (10 ,10.5, true);
+    let data: (i32, f64,bool , &str) = (10 ,10.5, true, "dimas");
     println!("{:?}",data);
+
+    let a = data.0;
+    let b = data.1;
+    let c = data.2;
+    let name = data.3;
+
+    println!("{}{}{}{}", a, b, c, name);
 }
