31fc9dae8587ea36440242beb335a580f679230a
pavelc4 <dimzzy23@gmail.com>
2025-10-10 05:08:53 +0700

    doc (array) example how to change data array on rust tike a tuple

--- src/bin/array.rs
+++ src/bin/array.rs
@@ -1,10 +1,20 @@
 fn main() {
-    let array: [i32; 4] = [1, 2, 3, 4];
+    let mut array: [i32; 5] = [1, 2, 3, 4, 5];
+    // let array: [i32; 4] = [1, 2, 3, 4];
     println!("{:?}", array);
 
-    let a = array[1];
-    let b = array[3];
+    let a = array[0];
+    let b = array[1];
 
     println!("{}", a);
     println!("{}", b);
+
+    array[0] = 20;
+    array[1] = 10;
+
+    let a_baru = array[0];
+    let b_baru = array[1];
+
+    println!("{:?}", a_baru);
+    println!("{:?}", b_baru);
 }
