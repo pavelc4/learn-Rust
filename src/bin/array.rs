00004a459e0cf41737a393b511c6fabd8b2895e7
pavelc4 <dimzzy23@gmail.com>
2025-10-10 04:52:16 +0700

    doc (array) example how to acces array on rust

--- src/bin/array.rs
+++ src/bin/array.rs
@@ -1,4 +1,10 @@
 fn main() {
     let array: [i32; 4] = [1, 2, 3, 4];
     println!("{:?}", array);
+
+    let a = array[1];
+    let b = array[3];
+
+    println!("{}", a);
+    println!("{}", b);
 }
