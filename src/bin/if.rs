c81b0df50eff246034c6c85b271526ac0bbf8bfa
pavelc4 <dimzzy23@gmail.com>
2025-10-21 11:26:29 +0700

    feat(rust): add example demonstrating conditional statements
    Added a simple Rust example using if statements:
    
    Checks if values is greater than or equal to 9
    
    Prints a message when the condition is true
    
    Includes an equality check for values == 10
    
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

---
+++ src/bin/if.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let values = 10;
+
+    if values >= 9 {
+        println!("lebih kecil 1 angka dari {}", values);
+    }
+    if values == 10 {
+        println!("value sama dengan = {}", values);
+    }
+}
