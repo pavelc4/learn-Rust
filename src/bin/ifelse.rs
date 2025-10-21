7d3954ab963f9ae58ec72d7f31b2df22c680af47
pavelc4 <dimzzy23@gmail.com>
2025-10-21 11:26:55 +0700

    feat(rust): add example demonstrating if-else control flow
    Added a Rust example showing how to use if, else if, and else blocks:
    
    Checks whether values is greater than or equal to 9
    
    Compares equality with 8
    
    Uses else to handle cases where previous conditions are false
    
    Signed-off-by: pavelc4 <dimzzy23@gmail.com>

---
+++ src/bin/ifelse.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let values = 10;
+
+    if values >= 9 {
+        println!("lebih kecil 1 angka dari {}", values);
+    }
+    if values == 8 {
+        println!("value sama dengan = {}", values);
+    } else {
+        println!("value lebih kecil dari {}", values);
+    }
+}
